ba08297b364d43821ded6355bcf5f34b1686f3d9
Paul Chiusano <paul.chiusano@gmail.com>
2014-02-11 01:21:24 -0500

    simplified Client

--- src/main/scala/Client.scala
+++ src/main/scala/Client.scala
@@ -4,40 +4,42 @@ import scalaz.concurrent.Task
 import scalaz.Monad
 import scodec.Codec
 
-trait Client[F[_]] {
+trait Client {
 
-  def eval[A](a: Remote[A]): F[A]
-  def run[A](f: F[A]): A
-
-  def force[A](a: Remote[A]): A = run { eval(a) }
   def local[A:Codec:ClassManifest](a: A): Remote[A] =
     Remote.Local(a, Codec[A], implicitly[ClassManifest[A]])
+
   def async[A:Codec:ClassManifest](a: Task[A]): Remote[A] =
     Remote.Async(a, Codec[A], implicitly[ClassManifest[A]])
 
-  val syntax: ClientSyntax[F] = ClientSyntax(this)
+  val syntax: ClientSyntax = ClientSyntax(this)
 }
 
+trait FactorialClient extends Client {
+  def factorial: Remote[Int => Int] = Remote.Ref("factorial")
+}
+
+// could write a little tool to generate the client using reflection
+
 object Client {
-  def local(env: PartialFunction[String, Any]): Client[Task] = new Client[Task] {
+  def local[A](env: PartialFunction[String, Any])(r: Remote[A]): Task[A] = {
     import Remote._
     val T = Monad[Task]
-
-    def run[A](t: Task[A]): A = t.run
-
-    def eval[A](r: Remote[A]): Task[A] = r match {
+    r match {
       case Async(a, _, _) => a
       case Local(a,_,_) => Task.now(a)
       case Ref(name) => env.lift(name) match {
         case None => Task.delay { sys.error("Unknown name on server: " + name) }
         case Some(a) => Task.now(a.asInstanceOf[A])
       }
-      case Ap1(f,a) => T.apply2(eval(f), eval(a))(_(_))
-      case Ap2(f,a,b) => T.apply3(eval(f), eval(a), eval(b))(_(_,_))
-      case Ap3(f,a,b,c) => T.apply4(eval(f), eval(a), eval(b), eval(c))(_(_,_,_))
-      case Ap4(f,a,b,c,d) => T.apply5(eval(f), eval(a), eval(b), eval(c), eval(d))(_(_,_,_,_))
+      case Ap1(f,a) => T.apply2(local(env)(f), local(env)(a))(_(_))
+      case Ap2(f,a,b) => T.apply3(local(env)(f), local(env)(a), local(env)(b))(_(_,_))
+      case Ap3(f,a,b,c) => T.apply4(local(env)(f), local(env)(a), local(env)(b), local(env)(c))(_(_,_,_))
+      case Ap4(f,a,b,c,d) => T.apply5(local(env)(f), local(env)(a), local(env)(b), local(env)(c), local(env)(d))(_(_,_,_,_))
     }
   }
+
+
 }
 
 object ClientExample {
@@ -45,7 +47,7 @@ object ClientExample {
   import Remoteable._
   import Codecs._
 
-  val c: Client[Task] = ???
+  val c: Client = ???
   import c.syntax._
   val fac: Remote[Int => Int] = ???
   val gcd: Remote[(Int,Int) => Int] = ???
@@ -54,7 +56,7 @@ object ClientExample {
   val ar2: Remote[Int] = ar
 }
 
-case class ClientSyntax[F[_]](C: Client[F]) {
+case class ClientSyntax(C: Client) {
 
   implicit class Ap1[A,B:Remoteable](self: Remote[A => B]) {
     def apply(a: Remote[A]): Remote[B] =
