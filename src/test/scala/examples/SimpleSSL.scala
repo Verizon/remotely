d29f09a4abf9c558f989f782e3472455de4b9530
Paul Chiusano <paulx.chiusano@intel.com>
2014-04-09 14:08:58 -0700

    Refactoring of ssl test

--- src/test/scala/examples/SimpleSSL.scala
+++ src/test/scala/examples/SimpleSSL.scala
@@ -19,18 +19,22 @@ object SimpleSSL extends App {
   // to actually run a remote expression, we need an endpoint
   implicit val clientPool = akka.actor.ActorSystem("rpc-client")
 
-  val expr: Remote[Int] = sum(List(0,1,2,3,4))
-  val loc: Endpoint = Endpoint.singleSSL(tls.default)(addr) // takes ActorSystem implicitly
-  val result: Task[Int] = expr.run(loc, Monitoring.consoleLogger("[client]"))
+  try {
+    val expr: Remote[Int] = sum(List(0,1,2,3,4))
+    val loc: Endpoint = Endpoint.singleSSL(tls.default)(addr) // takes ActorSystem implicitly
+    val result: Task[Int] = expr.run(loc, Monitoring.consoleLogger("[client]"))
 
-  // running a couple times just to see the latency improve for subsequent reqs
-  try println { result.run; result.run; result.run }
+    // running a couple times just to see the latency improve for subsequent reqs
+    println { result.run; result.run; result.run }
+  }
   finally {
     // hack to allow asynchronous actor shutdown messages to propagate,
     // without this, we get some dead letter logging
     // I'm sure there's a better way to do this
+    println("shutting down...")
     Thread.sleep(1000)
     server()
     clientPool.shutdown()
+    println("..done shutting down")
   }
 }
