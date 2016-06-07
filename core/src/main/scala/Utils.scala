5c037db7fb9633990b2730f971f0a18b95fffd69
Alex Henning Johannessen <ahjohannessen@gmail.com>
2016-06-07 17:09:00 +0100

    deps: downgrade scodec
     - downgrade scodec-core and scodec-scalaz because
       present versions are incompatible with our dependency
       on scalaz-stream with respect to scodec-bits.
    
     - add `AugmentedAttempt` implicit helper class back that
       has `toTask` method. This can be removed when we can move
       scodec forward.
    
     - NOTE: we can upgrade as soon scalaz-stream moves forward
             with respect to scodec.

--- core/src/main/scala/Utils.scala
+++ core/src/main/scala/Utils.scala
@@ -19,16 +19,25 @@ package remotely
 import remotely.codecs.DecodingFailure
 import scodec.Attempt.{Successful, Failure}
 import scodec.{Attempt, Err}
-
 import scalaz.{\/-, -\/, \/}
 import scalaz.concurrent.Task
 
 package object utils {
+
   implicit class AugmentedEither[E,A](a: E \/ A) {
     def toTask(implicit conv: E => Throwable): Task[A] = a match {
       case -\/(e) => Task.fail(conv(e))
-      case \/-(a) => Task.now(a)
+      case \/-(s) => Task.now(s)
     }
   }
-  implicit def errToE(err: Err) = new DecodingFailure(err)
+
+  implicit def errToE(err: Err): DecodingFailure = new DecodingFailure(err)
+
+  implicit class AugmentedAttempt[A](a: Attempt[A]) {
+    def toTask(implicit conv: Err => Throwable): Task[A] = a match {
+      case Failure(err)  => Task.fail(conv(err))
+      case Successful(s) => Task.now(s)
+    }
+  }
+
 }
